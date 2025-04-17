46ad28a5a904c5059e2c66dd6b4c0602680b41fa
Vedant72168 <vedantpawar.skn.comp@gmail.com>
2025-04-17 19:01:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
