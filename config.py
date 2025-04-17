d361ebdb0cb066f9971f44d0d22a20ebaa341a10
Vedant72168 <vedantpawar.skn.comp@gmail.com>
2025-04-17 18:44:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'your_secret_key_here'  # Used for session security
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:password123@localhost/parking_db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/parking.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
