9545d75eeb5ec02eee285e9d2a9013e526407652
Vedant72168 <vedantpawar.skn.comp@gmail.com>
2025-04-17 19:11:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SECRET_KEY = 'your_secret_key_here'  # Used for session security
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/parking.db'
+    SECRET_KEY = 'your_super_secret_key'  # You can set anything here or use os.urandom(24).hex()
+    
+    # SQLite Database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
